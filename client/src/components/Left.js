4b7d3ad6b9134ccb0df0370ce3c5ab12e203ecb2
Brandon Benefield <bsquared18@gmail.com>
2017-10-15 13:06:10 -0400

    adjust noteList functionality, change styles

--- client/src/components/Left.js
+++ client/src/components/Left.js
@@ -4,10 +4,17 @@ import Input from '../containers/Input';
 import NoteList from '../containers/NoteList';
 
 const Left = () => {
+  const handleNoteListWidth = () => {
+    document.querySelector('.buns').addEventListener('click', () => {
+      document.querySelector('.left').classList.toggle('left-small');
+    });
+  }
+  
   return (
     <div className='left'>
       <header>
         <span className='note-list-title'>Notes</span>
+        <span className='buns' onClick={() => handleNoteListWidth()}>☰</span>
       </header>
       
       <Input />
--- client/src/containers/TextArea.js
+++ client/src/containers/TextArea.js
@@ -12,7 +12,11 @@ class TextArea extends Component {
     const { noteData, currentNote } = this.props.input;
     
     return (
-      <textarea value={noteData[currentNote]} onChange={e => this.handleNoteData(e)} />
+      <textarea 
+        value={noteData[currentNote]}
+        onChange={e => this.handleNoteData(e)}
+        placeholder={'  Welcome to Notes! Let us be your second brain. Never forget another thing!\n\n  To start, click on the box to your left and name your note. From there you can start typing here on the right and record all of your thoughts.\n\n  If you need more room, go ahead and click the \'☰\' on the left to give yourself some breathing room.'}
+      />
     );
   }
 };
