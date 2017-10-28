b563f226135cc6ed1cc07f571294dd89fbe69a63
Brandon Benefield <bsquared18@gmail.com>
2017-10-28 14:40:46 -0400

    feat(LandingPage): create LandingPage component as the landing page of app

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {  
+  return (
+    <div>
+      <nav className='landing-page-nav'>
+        <span className='take-notes-logo'>Take Notes!</span>
+        <ul className='landing-page-nav-links' style={{ display: 'inline' }}>
+          <li>Sign Up</li>
+          <li>Log In</li>
+        </ul>
+      </nav>
+      
+      <main>
+        <h1>Welcome to Take Notes! The easiest way to never forget!</h1>
+        
+        <p>
+          Organize your life. Important work event next month? Birthday party? Never 
+          forget another important date with the help of notifications sent to you a week 
+           or the day before your next event.
+        </p>
+        <Link to='/notes'>
+          <button type='button'>Start taking notes</button>
+        </Link>
+      </main>
+      
+    </div>
+  );
+};
+
+export default LandingPage;
