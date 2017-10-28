c81bb926fdc9935b098fbfc0c520cb477cc3bb68
Brandon Benefield <bsquared18@gmail.com>
2017-10-28 14:40:04 -0400

    feat(Notes): create new component to hold Left & Right

---
+++ client/src/components/Notes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Left from './Left';
+import Right from './Right';
+
+const Notes = () => {
+  return (
+    <div>
+      <Left />
+      <Right />
+    </div>
+  );
+};
+
+export default Notes;
