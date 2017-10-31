260e7d442900a51dd44a84450fc275d2b7c2924c
Brandon Benefield <bsquared18@gmail.com>
2017-10-31 00:12:58 -0400

    maintenance

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // This is the main starting point for the app
 // We need to require in app dependencies before starting
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // tells the server that incoming data should use JSON
 app.use(bodyParser.json());
+// tells express where to serve our frontend from
+app.use(express.static(path.join(__dirname, 'client/build')));
+routes(app);
 
 // this is going to get our backend server up and running
 app.listen(PORT, () => {
