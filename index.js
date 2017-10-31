77310193ace063cc5265ae463a5586dd1db33e85
Brandon Benefield <bsquared18@gmail.com>
2017-10-31 10:10:49 -0400

    refactor(index.js): split routes to seperate file `routes.js`

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// tells the server that incoming data should use JSON
+// alot of info around `bodyParser` for now, just know that it creates a new object on the `req` object
+// i.e. `req.body.`
 app.use(bodyParser.json());
 // tells express where to serve our frontend from
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// grabs all the routing info from `./routes.js`
 routes(app);
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
 
 // this is going to get our backend server up and running
 app.listen(PORT, () => {
